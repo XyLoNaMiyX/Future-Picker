c12fb947ee8a09eaa2b581503e365fe7f9651f1c
Lonami Exo <totufals@hotmail.com>
2017-11-02 13:04:48 +0100

    Initial commit

---
+++ Future Picker/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Future_Picker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startB = new System.Windows.Forms.Button();
+            this.questionL = new System.Windows.Forms.Label();
+            this.dislikeB = new System.Windows.Forms.Button();
+            this.neutralB = new System.Windows.Forms.Button();
+            this.likeB = new System.Windows.Forms.Button();
+            this.currentQuestionL = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.scorePB = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.acceptB = new System.Windows.Forms.Button();
+            this.becomeL = new System.Windows.Forms.Label();
+            this.scoresLV = new System.Windows.Forms.ListView();
+            this.fieldCH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.scoreCH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.checkUpdatesB = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scorePB)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startB
+            // 
+            this.startB.Location = new System.Drawing.Point(3, 3);
+            this.startB.Name = "startB";
+            this.startB.Size = new System.Drawing.Size(164, 23);
+            this.startB.TabIndex = 0;
+            this.startB.TabStop = false;
+            this.startB.Text = "Comenzar test";
+            this.startB.UseVisualStyleBackColor = true;
+            this.startB.Click += new System.EventHandler(this.startB_Click);
+            // 
+            // questionL
+            // 
+            this.questionL.AutoSize = true;
+            this.questionL.Location = new System.Drawing.Point(3, 3);
+            this.questionL.Name = "questionL";
+            this.questionL.Size = new System.Drawing.Size(165, 13);
+            this.questionL.TabIndex = 1;
+            this.questionL.Text = "Inicializando zona de preguntas...";
+            // 
+            // dislikeB
+            // 
+            this.dislikeB.Location = new System.Drawing.Point(3, 55);
+            this.dislikeB.Name = "dislikeB";
+            this.dislikeB.Size = new System.Drawing.Size(75, 23);
+            this.dislikeB.TabIndex = 2;
+            this.dislikeB.Text = "No me gusta";
+            this.dislikeB.UseVisualStyleBackColor = true;
+            this.dislikeB.Click += new System.EventHandler(this.dislikeB_Click);
+            // 
+            // neutralB
+            // 
+            this.neutralB.Location = new System.Drawing.Point(3, 84);
+            this.neutralB.Name = "neutralB";
+            this.neutralB.Size = new System.Drawing.Size(164, 23);
+            this.neutralB.TabIndex = 3;
+            this.neutralB.Text = "Me es indiferente/desconozco";
+            this.neutralB.UseVisualStyleBackColor = true;
+            this.neutralB.Click += new System.EventHandler(this.neutralB_Click);
+            // 
+            // likeB
+            // 
+            this.likeB.Location = new System.Drawing.Point(92, 55);
+            this.likeB.Name = "likeB";
+            this.likeB.Size = new System.Drawing.Size(75, 23);
+            this.likeB.TabIndex = 4;
+            this.likeB.Text = "Me gusta";
+            this.likeB.UseVisualStyleBackColor = true;
+            this.likeB.Click += new System.EventHandler(this.likeB_Click);
+            // 
+            // currentQuestionL
+            // 
+            this.currentQuestionL.AutoSize = true;
+            this.currentQuestionL.Location = new System.Drawing.Point(3, 36);
+            this.currentQuestionL.Name = "currentQuestionL";
+            this.currentQuestionL.Size = new System.Drawing.Size(76, 13);
+            this.currentQuestionL.TabIndex = 5;
+            this.currentQuestionL.Text = "Pregunta 1/60";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.startB);
+            this.panel1.Location = new System.Drawing.Point(217, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 34);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.scorePB);
+            this.panel2.Controls.Add(this.questionL);
+            this.panel2.Controls.Add(this.dislikeB);
+            this.panel2.Controls.Add(this.currentQuestionL);
+            this.panel2.Controls.Add(this.neutralB);
+            this.panel2.Controls.Add(this.likeB);
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(12, 110);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(500, 171);
+            this.panel2.TabIndex = 7;
+            this.panel2.Visible = false;
+            // 
+            // scorePB
+            // 
+            this.scorePB.Location = new System.Drawing.Point(173, 55);
+            this.scorePB.Name = "scorePB";
+            this.scorePB.Size = new System.Drawing.Size(50, 50);
+            this.scorePB.TabIndex = 6;
+            this.scorePB.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.checkUpdatesB);
+            this.panel3.Controls.Add(this.acceptB);
+            this.panel3.Controls.Add(this.becomeL);
+            this.panel3.Controls.Add(this.scoresLV);
+            this.panel3.Location = new System.Drawing.Point(518, 26);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(265, 315);
+            this.panel3.TabIndex = 8;
+            this.panel3.Visible = false;
+            // 
+            // acceptB
+            // 
+            this.acceptB.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.acceptB.Location = new System.Drawing.Point(146, 242);
+            this.acceptB.Name = "acceptB";
+            this.acceptB.Size = new System.Drawing.Size(74, 23);
+            this.acceptB.TabIndex = 2;
+            this.acceptB.Text = "Aceptar y salir";
+            this.acceptB.UseVisualStyleBackColor = true;
+            this.acceptB.Click += new System.EventHandler(this.acceptB_Click);
+            // 
+            // becomeL
+            // 
+            this.becomeL.AutoSize = true;
+            this.becomeL.Location = new System.Drawing.Point(3, 5);
+            this.becomeL.Name = "becomeL";
+            this.becomeL.Size = new System.Drawing.Size(105, 13);
+            this.becomeL.TabIndex = 1;
+            this.becomeL.Text = "¡Deberías hacerte...!";
+            // 
+            // scoresLV
+            // 
+            this.scoresLV.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fieldCH,
+            this.scoreCH});
+            this.scoresLV.FullRowSelect = true;
+            this.scoresLV.GridLines = true;
+            this.scoresLV.Location = new System.Drawing.Point(2, 24);
+            this.scoresLV.Name = "scoresLV";
+            this.scoresLV.Size = new System.Drawing.Size(218, 212);
+            this.scoresLV.TabIndex = 0;
+            this.scoresLV.UseCompatibleStateImageBehavior = false;
+            this.scoresLV.View = System.Windows.Forms.View.Details;
+            // 
+            // fieldCH
+            // 
+            this.fieldCH.Text = "Campo";
+            this.fieldCH.Width = 183;
+            // 
+            // scoreCH
+            // 
+            this.scoreCH.Text = "Pts";
+            this.scoreCH.Width = 28;
+            // 
+            // checkUpdatesB
+            // 
+            this.checkUpdatesB.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkUpdatesB.Location = new System.Drawing.Point(6, 243);
+            this.checkUpdatesB.Name = "checkUpdatesB";
+            this.checkUpdatesB.Size = new System.Drawing.Size(134, 23);
+            this.checkUpdatesB.TabIndex = 3;
+            this.checkUpdatesB.Text = "Comprobar actualizaciones";
+            this.checkUpdatesB.UseVisualStyleBackColor = true;
+            this.checkUpdatesB.Click += new System.EventHandler(this.checkUpdatesB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 372);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(200, 80);
+            this.Name = "Form1";
+            this.Text = "Futuro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scorePB)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startB;
+        private System.Windows.Forms.Label questionL;
+        private System.Windows.Forms.Button dislikeB;
+        private System.Windows.Forms.Button neutralB;
+        private System.Windows.Forms.Button likeB;
+        private System.Windows.Forms.Label currentQuestionL;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox scorePB;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListView scoresLV;
+        private System.Windows.Forms.ColumnHeader fieldCH;
+        private System.Windows.Forms.ColumnHeader scoreCH;
+        private System.Windows.Forms.Label becomeL;
+        private System.Windows.Forms.Button acceptB;
+        private System.Windows.Forms.Button checkUpdatesB;
+    }
+}
+
---
+++ Future Picker/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Imaging;
+
+namespace Future_Picker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Variables
+
+        public class Question {
+            public string Quest { get; set; }
+            public int Score { get; set; }
+
+            public Question(string question, int score) {
+                Quest = question;
+                Score = score;
+            }
+        }
+
+        string[] questionsStrings = new string[] {
+            "Examinar, analizar y estudiar el funcionamiento de máquinas nuevas e inventos tecnológicos.",
+            "Reparar equipos de sonido, ordenadores, televisores, frigoríficos, aire acondicionado, etc.",
+            "Intervenir en la elaboración de contratos, escrituras y testamentos.",
+            "Diagnosticar y administrar tratamientos médicos, para curar o prevenir las enfermedades de los seres humanos.",
+            "Traducir e interpretar textos escritos en otro idioma.",
+            "Relizar funciones de protección, seguridad y vigilancia",
+            "Desarrollar y realizar un guión cinematográfico.",
+            "Realizar estudios en el área de las matemáticas y la estadística.",
+            "Componer, dirigir o interpretar obras musicales.",
+            "Estudiar la evolución de las razas humanas, su organización política, social, econónmica y cultural (estilos artísticos, etc.).",
+            "Realizar estudios e investigaciones sobre diferentes rocas.",
+            "Manejar máquinas de oficina (ordenador, calculadoras, etc.) y comunicaciones telefónicas.",
+            "Planificar, organizar, dirigir y controlar las actividades de una empresa.",
+            "Trabajar e interesar,e por las explotaciones ganaderas y resolver sus problemas.",
+            "Clasificiar documentos, cartas, sellos, diapositivas u otros objetos por temas.",
+            "Entrenar o preparar a deportistas para mejorar su rendimiento.",
+            "Informar de los acontecimientos de la actualidad.",
+            "Indagar y descubrir por qué de las teorías científicas.",
+            "Diseñar muebles, cerámica, complementos, prendas de vestir, joyas, etc.",
+            "Participar en la política regional o nacional.",
+            "Diseñar, proyectar y elaborar los planos de un edificio en la ciudad.",
+            "Construir o reparar muebles u objetos de madera.",
+            "Dirigir o participar en las actividades de un banco.",
+            "Diagnosticar, prevenir y tratar las enfermedades y/o lesiones de los animales.",
+            "Estudiar el origen y evolución de las lenguas.",
+            "Perseguir, detener y poner a disposición judicial a aquellos que cometen delitos.",
+            "Crear o diseñar anuncios publiciatrios.",
+            "Realizar experimentos para analizar y estudiar los fenómenos químicos y bioquímicos.",
+            "Dar ercitales de canto y música.",
+            "Dar clases en un colegio.",
+            "Dirigir la instalación de sistemas de alarmas.",
+            "Realizar actividades donde se requiera habilidad manual y práctoca.",
+            "Intervenir ante los tribunales de justicia en nombre de la ley, representando a un cliente.",
+            "Organizar una finca supervisando el cuidado de los animales.",
+            "Ordenar, clasificar y archivar documentos e informes.",
+            "Dirigir las competiciones deportivas y aplicar las reglas establecidas.",
+            "Componer, dirigir o interpretar una representación teatral, televisiva o cinematográfica.",
+            "Hacer descubrimientos científicos.",
+            "Restaurar obras de arte, tomando decisiones propias sobre la forma, modo y técnica a aplicar.",
+            "Investigar los problemas psicológicos de las personas.",
+            "Estudiar, proyectar y construir instalaciones eléctricas, puentes, túneles, etc.",
+            "Ajustar maquinaria e instalar equipos eléctricos en fábricas o edificios.",
+            "Asesorar sobre problemas contables, financieros y/o económicos.",
+            "Investigar en un laboratorio el origen de las enfermedades.",
+            "Organizar y clasificar libros y documentos en una biblioteca.",
+            "Prestar servicios en organismos y establecimientos militares.",
+            "Presentar las noticias en los informativos de tv.",
+            "Organizar la repoblación del monte seleccionando las plantas más adecuadas.",
+            "Crear y ejecutar pasos de danza.",
+            "Estudiar teorías relativas al compotramiento del ser humano.",
+            "Elaborar y diseñar programas informáticos.",
+            "Reparar las averías de los vehículos.",
+            "Aplicar los principios de la teoría económica para solcuionar problemas financieros en empresas.",
+            "Prescribir medicamentos para la curación de enfermedades.",
+            "Traducir textos de diferentes idiomas.",
+            "Organizar y planificar competiciones deportivas.",
+            "Redactar noticias, comentar informaciones y coordinar la redacción de una publicación.",
+            "Investigar en un laboratorio para la creación de nuevos materiales.",
+            "Fotografiar, esculpir o pintar creaciones artísticas.",
+            "Orientar profesional y/o académicamente a las personas."
+        };
+
+        string[] fields = new string[10] {
+            "Científico técnico",
+            "Técnico aplicado",
+            "Jurídico Administrativo",
+            "Bio-sanitario",
+            "Lingüistico literario",
+            "Físico deportivo",
+            "Publicidad y medios de comunicación",
+            "Científico experimental",
+            "Artístico musical",
+            "Ciencias sociales"
+        };
+
+        Point p0 = new Point(9, 9);
+
+        List<Question> questions = new List<Question>();
+
+        #endregion
+
+
+        //Max 6 pts.
+        //Campo 1: 1-11-21-31-41-51
+        //Campo 2: 2-12-22-32-42-52
+        
+        //Campos 1                      2                   3                           4               5                        6                   7                                      8                           9                       10
+        //Campos Científico-técnico     Técnico-aplicado    Jurídico-administrativo     Bio-sanitario    Lingüistico y literario  Físico-deportivo    Publicidad y medios de comunicación    Científico-experimental     Artístico y musical     Ciencias sociales
+
+        //-6 _ -3   = Baja
+        //-2 _ 2    = Media
+        // 3 _ 6    = Alta
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Location = p0;
+            this.Size = this.MinimumSize;
+        }
+
+        private void startB_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        #region Quiz manager
+
+        int q = 0;
+
+        private void Start() {
+            startB.Enabled = false;
+            startB.Text = "El test ha comenzado";
+            this.Text = "Elige tu futuro";
+
+            panel1.Visible = false;
+            panel2.Visible = true;
+            panel2.Location = p0;
+
+            Ease();
+        }
+
+        private void DoneStart()
+        {
+            this.Size = panel2.Size;
+            questionL.Text = questionsStrings[q];
+            questionL.MaximumSize = new Size(this.Size.Width - 40, 40);
+            panel2.Enabled = true;
+
+            tp.Interval = 2;
+            tp.Enabled = true;
+            tp.Tick += tp_Tick;
+        }
+
+        int sen = 0;
+
+        Timer tf = new Timer();
+
+        private void Ease() {
+            tf.Interval = 5;
+            tf.Enabled = true;
+            tf.Tick += t_Tick;
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            double s = Math.Sin(Math.PI * sen / 180);
+
+            if (sen < 90)
+                sen++;
+            else
+            {
+                DoneStart();
+                tf.Dispose();
+            }
+
+            double width = s * panel2.Size.Width;
+            double height = s * panel2.Size.Height;
+            this.Size = new Size((int)width, (int)height);
+        }
+
+        private void NextQuestion(int score) {
+            questions.Add(new Question(questionsStrings[q], score));
+            q++;
+
+            if (q < questionsStrings.Length)
+            {
+                questionL.Text = questionsStrings[q];
+                currentQuestionL.Text = "Pregunta " + (q + 1) + "/60";
+            }
+            else
+                FinishQuiz();
+        }
+
+        private void FinishQuiz() {
+            panel2.Visible = false;
+            panel3.Location = p0;
+            panel3.Visible = true;
+            this.Size = panel3.Size;
+
+            GetScore();
+        }
+
+        #endregion
+
+        #region Like buttons and effects
+
+        Timer tp = new Timer();
+        Image currentImg = Properties.Resources.Neutral;
+        float fade = 0;
+
+        private void dislikeB_Click(object sender, EventArgs e)
+        {
+            NextQuestion(-1);
+            tp.Enabled = true;
+            fade = 100f;
+            SetImg(Properties.Resources.Dislike);
+        }
+
+        private void neutralB_Click(object sender, EventArgs e)
+        {
+            NextQuestion(0);
+            tp.Enabled = true;
+            fade = 100f;
+            SetImg(Properties.Resources.Neutral);
+        }
+
+        private void likeB_Click(object sender, EventArgs e)
+        {
+            NextQuestion(+1);
+            SetImg(Properties.Resources.Like);
+        }
+
+        private void SetImg(Image img)
+        {
+            tp.Enabled = true;
+            fade = 100f;
+            currentImg = img;
+        }
+
+        void tp_Tick(object sender, EventArgs e)
+        {
+            if (fade > 0)
+                fade--;
+            else
+                tp.Enabled = false;
+
+            scorePB.Image = ImageTransparency.ChangeOpacity(currentImg, fade / 100);
+        }
+
+        #endregion
+
+        #region Score
+
+        private void GetScore() {
+            int[] scores = new int[10];
+
+            for (int i = 0; i < questions.Count; i++)
+            {
+                Question q = questions[i];
+                string index = i.ToString();
+                string lstChar = index[index.Length - 1].ToString();
+                int ind = Int32.Parse(lstChar);
+                scores[ind] += q.Score;
+            }
+
+            string bf = "...nada! ¡Quédate con tus padres salao";
+            int bi = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (scores[i] > bi)
+                {
+                    bi = scores[i];
+                    bf = fields[i];
+                }
+
+                var item = new ListViewItem(new[] { fields[i], scores[i].ToString() });
+                scoresLV.Items.Add(item);
+            }
+
+            becomeL.Text = "¡Deberías hacerte " + bf.ToLower() + "!";
+        }
+
+        #endregion
+
+        private void acceptB_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkUpdatesB_Click(object sender, EventArgs e)
+        {
+            new UpdateChecker.UpdateChecker(System.Reflection.Assembly.GetExecutingAssembly().Location, "fup").Show();
+        }
+
+    }
+    class ImageTransparency
+    {
+        public static Bitmap ChangeOpacity(Image img, float opacityvalue)
+        {
+            Bitmap bmp = new Bitmap(img.Width, img.Height); // Determining Width and Height of Source Image
+            Graphics graphics = Graphics.FromImage(bmp);
+            ColorMatrix colormatrix = new ColorMatrix();
+            colormatrix.Matrix33 = opacityvalue;
+            ImageAttributes imgAttribute = new ImageAttributes();
+            imgAttribute.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            graphics.DrawImage(img, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAttribute);
+            graphics.Dispose();   // Releasing all resource used by graphics 
+            return bmp;
+        }
+    }
+}
